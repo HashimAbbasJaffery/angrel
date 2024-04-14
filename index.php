c8ec82db777625f9d70f15c6e509d278506b2a47
Hashim Abbas JAffery <habbas2121@outlook.com>
2024-04-14 14:53:24 +0500

    Environment File

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
 // use Core\Route;
+require_once "vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 spl_autoload_register(function($class) {
     $result = str_replace("\\", "/", $class);
